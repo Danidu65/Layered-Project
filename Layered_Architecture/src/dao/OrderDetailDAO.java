59debf3555dcf5b6f5f2a20e71a4dc96c978226a
Danidu65 <daniduijse65@Gmail.com>
2023-05-24 10:56:09 +0530

    Initial commit

---
+++ Layered_Architecture/src/dao/OrderDetailDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.OrderDetailDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface OrderDetailDAO {
+
+    public boolean saveOrderDetail(String orderId, List<OrderDetailDTO> orderDetails) throws SQLException;
+
+    }

046797d75028db8235bc48e16d36e6efbcf57ff5
Danidu65 <daniduijse65@Gmail.com>
2023-05-26 10:18:58 +0530

    Changed

---
+++ Layered_Architecture/src/dao/OrderDetailsDAO.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.OrderDetailDTO;
+
+import java.sql.SQLException;
+
+public interface OrderDetailsDAO extends CrudDAO<OrderDetailDTO>{
+//    public boolean saveOrderDetails(OrderDetailDTO dto) throws SQLException, ClassNotFoundException;
+}

215476908b9f6af85f10a6590e16ca0e422c96e3
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-05 22:53:41 +0530

    commit

--- Layered_Architecture/src/AppInitializer.java
+++ Layered_Architecture/src/AppInitializer.java
@@ -6,11 +6,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
-
 public class AppInitializer extends Application {
     public static void main(String[] args) {
         launch(args);
@@ -25,4 +20,5 @@ public class AppInitializer extends Application {
         primaryStage.centerOnScreen();
         primaryStage.show();
     }
+    //0715796089
 }
